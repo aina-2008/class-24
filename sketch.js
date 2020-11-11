233a3a73f7532e0854d4af63c9543de0adb724ce
aina-2008 <73205497+aina-2008@users.noreply.github.com>
2020-11-10 16:44:19 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,ground;
+
+
+
+
+function setup() {
+     createCanvas(400,400);
+    engine = Engine.create();//world
+    world = engine.world;
+
+    ground = new Ground(200,400,400,20)
+     
+    box1=new Box(200,200,50,50);
+    box2=new Box(180,100,50,50);
+    
+
+
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    
+  
+     box1.display();
+     box2.display();
+     ground.display();
+    }
